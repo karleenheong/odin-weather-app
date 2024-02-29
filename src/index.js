6eb1887bd78beedf2860ce2748d8ee189f2f022d
Karleen Heong <17855516+karleenheong@users.noreply.github.com>
2024-02-29 22:52:15 +0900

    Add api fetch request and process json

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
-
-  element.textContent = 'HELLO BITCH';
-  element.classList.add('hello');
-
-  return element;
+function processData(data) {
+  console.log(data.daily.temperature_2m_max);
 }
 
-document.body.appendChild(component());
+async function getWeather(lat, long) {
+  const str = `https://api.open-meteo.com/v1/forecast?latitude=${+lat}&longitude=${+long}&daily=temperature_2m_max&timezone=auto&forecast_days=3`;
+  try {
+    const response = await fetch(str, {mode: 'cors'});
+    const data = await response.json();
+    // process json data
+    processData(data);
+  } catch(e) {
+    console.error(e.message);
+  }
+}
+
+getWeather(35.21, 129.0689);
+// function component() {
+//   const element = document.createElement('div');
+
+//   element.textContent = 'HELLO BITCH';
+//   element.classList.add('hello');
+
+//   return element;
+// }
+
+// document.body.appendChild(component());
